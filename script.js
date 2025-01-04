96dbe1136b0af653ca7f4fcdea9bdcf9aa3607f3
dickyzuerst <dzuerst@gmail.com>
2025-01-04 14:45:32 +0700

    menampilkan data dari localstorage dengan manipulasi dom

--- script.js
+++ script.js
@@ -13,13 +13,33 @@ console.log(todos[1].name)
 
 */
 
+// GET HTML ELEMENT TO GET MANIPULATED WITH JS USING DOM
 const taskInputElement = document.querySelector(".task-input input")
 
+const taskBox = document.querySelector(".task-box")
+
 function showTodo() {
+    let li = ""
     todos.forEach((item, i) => {
         // for testing purpose
         // console.log(`${i} : ${item.name}`)
+        li += `
+        <li class="task">
+                <label for="${i}">
+                    <input type="checkbox" id="1">
+                    <p>${item.name}</p>
+                </label>
+                <div class="settings">
+                    <i class="uil uil-ellipsis-h"></i>
+                    <ul class="task-menu">
+                        <li><i class="uil uil-pen"></i>Edit</li>
+                        <li><i class="uil uil-trash"></i>Hapus</li>
+                    </ul>
+                </div>
+            </li>
+        `
     });
+    taskBox.innerHTML = li
 }
 showTodo()
 
